a3fc42c73c8d8b69b9af81be20fdb4a5a1f93d44
Lucas Duarte <lucas.glockshuber@outlook.com>
2024-12-15 20:31:36 -0300

    teste

--- Interface/IEmailService.cs
+++ Interface/IEmailService.cs
@@ -1,12 +1,11 @@
 ﻿using DesafioBtInoa.Model;
 using System.Threading.Tasks;
-using static Program;
 
 namespace AutomationEmail.Providers
 {
     public interface IEmailService
     {
-        Task<bool> EnviaEmailCompraVendaAtivo(DetalheCotacaoAtivo detalheCotacaoAtivo);
-        Task<bool> ConfiguracaoApiGmail();
+        Task EnviaEmailCompraVendaAtivo(DetalheCotacaoAtivo detalheCotacaoAtivo);
+        Task ConfiguracaoApiGmail();
     }
 }
